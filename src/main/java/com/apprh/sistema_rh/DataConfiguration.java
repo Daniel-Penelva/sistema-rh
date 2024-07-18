f763350dae5a1e7cf5196c2819b5c0d05321b536
Daniel Penelva <d4n.andrade@gmail.com>
2024-07-18 03:14:01 -0300

    feat: add o método jpaVendorAdapter Este método configura um JpaVendorAdapter específico para o Hibernate e MySQL (ou MariaDB), definindo várias propriedades para controlar o comportamento do JPA e do provedor Hibernate.

--- src/main/java/com/apprh/sistema_rh/DataConfiguration.java
+++ src/main/java/com/apprh/sistema_rh/DataConfiguration.java
@@ -5,6 +5,9 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
 public class DataConfiguration {
@@ -18,6 +21,17 @@ public class DataConfiguration {
         dataSource.setPassword("root");
         return dataSource;
 
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();   
+        adapter.setDatabase(Database.MYSQL);                                   // Configura o banco de dados a ser usado como MySQL.
+        adapter.setShowSql(true);                                              // Hibernate exibi as instruções SQL geradas no console.
+        adapter.setGenerateDdl(true);                                          // Hibernate gera automaticamente as instruções DDL
+        adapter.setDatabasePlatform("org.hibernate.dialect.MariaDBDialect");   // Essa plataforma de banco de dados (dialeto) a ser usada. Neste caso, está especificando MariaDBDialect, que é apropriado para MariaDB, um derivado do MySQL.
+		adapter.setPrepareConnection(true);                                    // Configura se o Hibernate deve preparar a conexão
+		return adapter;
     }
 
 }
