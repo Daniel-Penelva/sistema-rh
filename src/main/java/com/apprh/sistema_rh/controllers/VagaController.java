5c4ae5b83ca626af5d379784c396a487710ee2c1
Daniel Penelva <d4n.andrade@gmail.com>
2024-07-19 02:47:00 -0300

    feat: add o método form() mapeando requisição HTTP POST Nesse método lida com submissão de um formulário de cadastro de vagas via requisição HTTP POST para a URL /cadastrarVaga. É utilizado a anotação @Valid indica que o objeto Vaga deve ser validado de acordo com as anotações definidas na Entidade Vaga. O objeto BindingResult é usado para verificar se houve erros de validação durante o mapeamento do formulário para o objeto Vaga. O objeto RedirectAttributes é usado para adicionar atributos de redirecionamento (flash attributes) que persistem apenas durante o redirecionamento.

--- src/main/java/com/apprh/sistema_rh/controllers/VagaController.java
+++ src/main/java/com/apprh/sistema_rh/controllers/VagaController.java
@@ -1,12 +1,16 @@
 package com.apprh.sistema_rh.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.apprh.sistema_rh.models.Vaga;
 import com.apprh.sistema_rh.repositories.CandidatoRepository;
 import com.apprh.sistema_rh.repositories.VagaRepository;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -20,6 +24,20 @@ public class VagaController {
     @RequestMapping(value = "/cadastrarVaga", method = RequestMethod.GET)
     public String form() {
         return "vaga/formVaga";
+    }
+
+    @RequestMapping(value = "/cadastrarVaga", method = RequestMethod.POST)
+    public String form(@Valid Vaga vaga, BindingResult bindingResult, RedirectAttributes attributes) {
+
+        if (bindingResult.hasErrors()) {
+            attributes.addFlashAttribute("mensagem", "Verifique os campos");
+            return "redirect:/cadastrarVaga";
+        }
+
+        vagaRepository.save(vaga);
+        attributes.addFlashAttribute("mensagem", "Vaga cadastrada com sucesso");
+        return "redirect:/cadastrarVaga";
+
     }
 
 }
