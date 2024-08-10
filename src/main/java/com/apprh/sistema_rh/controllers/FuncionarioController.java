773087067f51e3ab043d789ca7f8663dddbdb28a
Daniel Penelva <d4n.andrade@gmail.com>
2024-08-10 03:32:56 -0300

    feat: add na classe FuncionarioController o método form() mapeando requisição HTTP POST Este método é responsável por processar a submissão de um formulário para cadastrar um funcionário. O parâmetro value define o caminho da URL (/cadastrarFuncionario) que acionará este método, e method especifica que ele responde a requisições do tipo POST. Isso é típico para operações que alteram o estado do servidor, como a criação de novos registros. @Valid Funcionario funcionario: Este parâmetro representa o objeto Funcionario que será preenchido com os dados do formulário. A anotação @Valid indica que o Spring deve validar este objeto com base nas anotações de validação definidas na classe Funcionario. BindingResult bindingResult: Este parâmetro contém os resultados da validação do objeto funcionario. Se houver erros de validação, eles serão armazenados aqui. RedirectAttributes attributes: Este parâmetro é usado para passar atributos para a próxima requisição, especialmente útil para mensagens flash que não devem ser persistidas entre requisições. O método inicia verificando erros, se houver erro é adicionada aos atributos de redirecionamento e o usuário é redirecionado de volta para o formulário (redirect:/cadastrarFuncionario). Depois salva o funcionário. Após salvar o funcionário, uma mensagem de sucesso é adicionada aos atributos de redirecionamento e o usuário é redirecionado novamente para o formulário.

--- src/main/java/com/apprh/sistema_rh/controllers/FuncionarioController.java
+++ src/main/java/com/apprh/sistema_rh/controllers/FuncionarioController.java
@@ -1,12 +1,16 @@
 package com.apprh.sistema_rh.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.apprh.sistema_rh.models.Funcionario;
 import com.apprh.sistema_rh.repositories.DependentesRepository;
 import com.apprh.sistema_rh.repositories.FuncionarioRepository;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -20,5 +24,19 @@ public class FuncionarioController {
     @RequestMapping(value = "/cadastrarFuncionario", method = RequestMethod.GET)
     public String form(){
         return "funcionario/formFuncionario";
+    }
+
+    // Método para cadastrar o funcionario
+    @RequestMapping(value = "/cadastrarFuncionario", method = RequestMethod.POST)
+    public String form(@Valid Funcionario funcionario, BindingResult bindingResult, RedirectAttributes attributes ){
+
+        if (bindingResult.hasErrors()) {
+            attributes.addFlashAttribute("mensagem", "Verifique os campos");
+			return "redirect:/cadastrarFuncionario";
+        }
+
+        funcionarioRepository.save(funcionario);
+        attributes.addFlashAttribute("mensagem", "Funcionário cadastrado com sucesso!");
+		return "redirect:/cadastrarFuncionario";
     }
 }
