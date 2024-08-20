7882bf3d3836ff59908f7e45b7823ddfcf05ffd9
Daniel Penelva <d4n.andrade@gmail.com>
2024-08-20 02:40:18 -0300

    feat: add o método updateFuncionario no FuncionarioController Este método lida com a atualização dos dados de um funcionário. Ele é responsável por processar a submissão do formulário de edição de um funcionário. A anotação @Valid Funcionario funcionario indica que o objeto funcionario deve ser validado conforme as anotações de validação definidas na entidade Funcionario. O BindingResult armazena os resultados da validação. Ele permite verificar se ocorreram erros na validação e responder adequadamente. O RedirectAttributes é usado para adicionar atributos à resposta HTTP, permitindo que mensagens sejam passadas entre requisições, geralmente usadas em redirecionamentos. O método salva o o objeto funcionario no banco de dados. Depois adiciona uma mensagem de sucesso ao RedirectAttributes que será exibida após o redirecionamento. Depois obtém o ID do funcionário atualizado. Este ID será usado para redirecionar para a página de dependentes do funcionário. Depois converte o ID do funcionário de long para String. Por fim redireciona o usuário para a página de listagem de dependentes do funcionário após a atualização.

--- src/main/java/com/apprh/sistema_rh/controllers/FuncionarioController.java
+++ src/main/java/com/apprh/sistema_rh/controllers/FuncionarioController.java
@@ -104,6 +104,18 @@ public class FuncionarioController {
         mv.addObject("funcionario", funcionario);
         return mv;
     }
+
+    // http://localhost:8080/editar-funcionario - Método para editar um funcionário por id
+    @RequestMapping(value = "/editar-funcionario", method = RequestMethod.POST)
+    public String updateFuncionario(@Valid Funcionario funcionario,  BindingResult bindingResult, RedirectAttributes attributes){
+        
+        funcionarioRepository.save(funcionario);
+        attributes.addFlashAttribute("successs", "Funcionário alterado com sucesso!");
+
+        long idLong = funcionario.getId();
+        String id = "" + idLong;
+		return "redirect:/dependentes/" + id;
+    }
 }
 
 
